5ce37df7d206c1b4f7746831d5b59c72f0a303e8
klub-mukul <mukul@klubworks.com>
2023-05-03 14:37:31 +0530

    add DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'admin',
+        password: '',
+        database: 'tags',
+        entities: [],
+        synchronize: true,
+      }),
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){}
+}
